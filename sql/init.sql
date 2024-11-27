4ae43cd5966b44a33fefd58b10e3b93d3af39763
dyrnq <dyrnq@qq.com>
2024-11-27 09:30:39 +0800

    Add sql

---
+++ sql/init.sql
@@ -0,0 +1,67 @@
+create database IF NOT EXISTS db_420100;
+create database IF NOT EXISTS db_420200;
+
+
+use db_420100;
+create table t_user
+(
+    `id`     bigint(20) NOT NULL AUTO_INCREMENT,
+    `name`   varchar(50),
+    `create_time` datetime,
+    `age` INT,
+    `snow_id` bigint(20),
+    `city_code` varchar(50),
+    PRIMARY KEY (`id`)
+);
+
+create table t_order_202407
+(
+    `id`     bigint(20) NOT NULL AUTO_INCREMENT,
+    `name`   varchar(50),
+    `create_time` datetime,
+    `snow_id` bigint(20),
+    `city_code` varchar(50),
+    PRIMARY KEY (`id`)
+);
+
+create table t_order_202408
+(
+    `id`     bigint(20) NOT NULL AUTO_INCREMENT,
+    `user_id` bigint(20),
+    `order_num`  bigint(20),
+    `create_time` datetime,
+    `city_code` varchar(50),
+    PRIMARY KEY (`id`)
+);
+
+use db_420200;
+create table t_user
+(
+    `id`     bigint(20) NOT NULL AUTO_INCREMENT,
+    `name`   varchar(50),
+    `create_time` datetime,
+    `age` INT,
+    `snow_id` bigint(20),
+    `city_code` varchar(50),
+    PRIMARY KEY (`id`)
+);
+
+create table t_order_202407
+(
+    `id`     bigint(20) NOT NULL AUTO_INCREMENT,
+    `name`   varchar(50),
+    `create_time` datetime,
+    `snow_id` bigint(20),
+    `city_code` varchar(50),
+    PRIMARY KEY (`id`)
+);
+
+create table t_order_202408
+(
+    `id`     bigint(20) NOT NULL AUTO_INCREMENT,
+    `user_id` bigint(20),
+    `order_num`  bigint(20),
+    `create_time` datetime,
+    `city_code` varchar(50),
+    PRIMARY KEY (`id`)
+);
