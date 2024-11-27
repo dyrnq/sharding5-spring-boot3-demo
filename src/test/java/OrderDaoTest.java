4ec040951556fc1a6334108ba910b6c401c6f844
dyrnq <dyrnq@qq.com>
2024-11-27 14:01:23 +0800

    use spring-boot-starter-test replace junit4

--- src/test/java/OrderDaoTest.java
+++ src/test/java/OrderDaoTest.java
@@ -2,13 +2,13 @@ import com.company.common.utils.TimeUtils;
 import com.company.modules.demo.dao.OrderDao;
 import com.company.modules.demo.dao.UserInfoDao;
 import com.company.modules.demo.model.Order;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest(classes = com.company.MainApplication.class)
 public class OrderDaoTest {
 
--- src/test/java/UserInfoDaoTest.java
+++ src/test/java/UserInfoDaoTest.java
@@ -1,15 +1,15 @@
 import com.company.modules.demo.dao.OrderDao;
 import com.company.modules.demo.dao.UserInfoDao;
 import com.company.modules.demo.model.UserInfo;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest(classes = com.company.MainApplication.class)
 public class UserInfoDaoTest {
 
